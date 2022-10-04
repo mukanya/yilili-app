66d26282f28cf68b764ffa3de622511ed5355ddc
ajays1991 <ajays871@gmail.com>
2022-10-04 12:07:34 +0530

    auth api

---
+++ src/components/language-app-user-2.0-main/models/profileModel.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * User Schema
+ */
+var ProfileSchema = new Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: true
+  },
+  userType: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    required: true
+  },
+  hash_password: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model('Profile', ProfileSchema);
